4f41644f8a468e5ecb2266f20ba8e6bf4455bc19
BuildTools <unconfigured@null.spigotmc.org>
2024-05-26 21:27:23 +0200

    Initial commit

---
+++ src/main/java/lu/kolja/ultraapi/UltraAPI.java
@@ -0,0 +1,17 @@
+package lu.kolja.ultraapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UltraAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
