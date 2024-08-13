0b9250b8d38549659eedecce94fba3d38472cdb9
Kolja <contact@kolja.lu>
2024-08-13 02:36:16 +0200

    Jitpack fix

--- src/main/java/lu/kolja/ultraapi/UltraAPI.java
+++ src/main/java/lu/kolja/ultraapi/UltraAPI.java
@@ -1,9 +1,15 @@
 package lu.kolja.ultraapi;
 
 import dev.triumphteam.gui.components.GuiAction;
+import dev.triumphteam.gui.guis.Gui;
 import dev.triumphteam.gui.guis.GuiItem;
+import dev.triumphteam.gui.guis.PaginatedGui;
+import net.kyori.adventure.text.TextComponent;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,7 +27,22 @@ public final class UltraAPI extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
-    public GuiItem guiItemBuilder(Material material, String name, int amount, @Nullable List<String> lore, GuiAction<InventoryClickEvent> lambda) {
+    public static GuiItem guiItemBuilder(Material material, String name, int amount, @Nullable List<String> lore, GuiAction<InventoryClickEvent> lambda) {
         return GuiUtil.itemBuilder(material, name, amount, lore, lambda);
     }
+    public static Gui guiBuilder(TextComponent title, int rows) {
+        return GuiUtil.guiBuilder(title, rows);
+    }
+    public static PaginatedGui pagedGuiBuilder(TextComponent title, int rows) {
+        return GuiUtil.pagedGuiBuilder(title, rows);
+    }
+    public static String chatFormatter(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+    public static void registerEvents(Plugin pl, Listener... listener) {
+        for (Listener l : listener) {
+            pl.getServer().getPluginManager().registerEvents(l, pl);
+        }
+    }
+
 }
